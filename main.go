0ce0816b1dc3870fa012b9c6e60f7909ef1580d1
Jeroen Bobbeldijk <jeroen@klippa.com>
2023-01-04 12:05:12 +0100

    Move to a full Redis implementation

--- main.go
+++ main.go
@@ -2,55 +2,28 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"github.com/go-redis/redis/v8"
 	"net"
 	"net/http"
-	"net/url"
-	"os"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	pb "github.com/klippa-app/keda-celery-scaler/externalscaler"
 
-	"github.com/go-chi/stampede"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-var (
-	reqCache = stampede.NewCache(512, time.Second*5, 0)
-)
-
-type FlowerWorker struct {
-	Active       []interface{} `json:"active"`
-	ActiveQueues []struct {
-		Name string `json:"name"`
-	} `json:"active_queues"`
-	Stats struct {
-		Pool struct {
-			MaxConcurrency int `json:"max-concurrency"`
-		} `json:"pool"`
-	}
-}
-
-type FlowerWorkerResult = map[string]FlowerWorker
-type FlowerWorkerStatusResult = map[string]bool
-
-type FlowerQueueLength struct {
-	ActiveQueues []struct {
-		Name     string `json:"name"`
-		Messages int    `json:"messages"`
-	} `json:"active_queues"`
-}
-
 type ExternalScaler struct{}
-
-var FlowerAddress = ""
 
 func (e *ExternalScaler) IsActive(ctx context.Context, scaledObject *pb.ScaledObjectRef) (*pb.IsActiveResponse, error) {
 	queue := scaledObject.ScalerMetadata["queue"]
@@ -86,11 +59,7 @@ var flowerClient = http.Client{
 }
 
 func getLoad(ctx context.Context, queue string) (int64, error) {
-	totalWorkersAvailable, totalActiveTasks, err := getQueueWorkers(ctx, queue)
-	if err != nil {
-		return 0, err
-	}
-
+	totalWorkersAvailable, totalActiveTasks := getQueueWorkers(queue)
 	queueLength, err := getQueueLength(ctx, queue)
 	if err != nil {
 		return 0, err
@@ -117,154 +86,49 @@ func getLoad(ctx context.Context, queue string) (int64, error) {
 	return int64(load * float64(100)), nil
 }
 
-func loadWorkerInfo(ctx context.Context) (interface{}, error) {
-	// We need to add refresh so that we always have the latest info.
-	req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("%s/api/workers?refresh=1", FlowerAddress), nil)
-	if err != nil {
-		return nil, err
-	}
+func getQueueWorkers(queue string) (int64, int64) {
+	workerMapLock.Lock()
+	defer workerMapLock.Unlock()
 
-	resp, err := flowerClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-
-	payload := FlowerWorkerResult{}
-	err = json.Unmarshal(body, &payload)
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-
-	return payload, nil
-}
-
-func getQueueWorkers(ctx context.Context, queue string) (int64, int64, error) {
 	start := time.Now()
-
-	workerInfoData, err := reqCache.Get(ctx, "worker_info", loadWorkerInfo)
-	if err != nil {
-		return 0, 0, status.Error(codes.Internal, err.Error())
-	}
-
-	log.Tracef("Fetching worker info for queue %s took %s", queue, time.Since(start).String())
-
-	workerInfo := workerInfoData.(FlowerWorkerResult)
-
-	workerStatus, err := getQueueWorkerStatus(ctx, queue)
-	if err != nil {
-		return 0, 0, status.Error(codes.Internal, err.Error())
-	}
 
 	totalWorkersAvailable := int64(0)
 	totalActiveTasks := int64(0)
 
 	// Loop through all available workers.
-	for worker := range workerInfo {
-		if !(*workerStatus)[worker] {
-			continue
-		}
+	for worker := range celeryWorkers {
 
 		// Only count the workers that are listening to our queue.
 		shouldCountWorker := false
-		for i := range workerInfo[worker].ActiveQueues {
-			if workerInfo[worker].ActiveQueues[i].Name == queue {
+		for i := range celeryWorkers[worker].Queues {
+			if celeryWorkers[worker].Queues[i] == queue {
 				shouldCountWorker = true
 				break
 			}
 		}
 
 		if shouldCountWorker {
-			totalActiveTasks += int64(len(workerInfo[worker].Active))
-			totalWorkersAvailable += int64(workerInfo[worker].Stats.Pool.MaxConcurrency)
+			totalActiveTasks += celeryWorkers[worker].Active
+			totalWorkersAvailable += celeryWorkers[worker].Concurrency
 		}
 	}
 
 	log.Tracef("Calculating worker and task counts for queue %s took %s", queue, time.Since(start).String())
 
-	return totalWorkersAvailable, totalActiveTasks, nil
-}
-
-func loadQueueWorkerStatus(ctx context.Context) (interface{}, error) {
-	req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("%s/api/workers?status=1", FlowerAddress), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := flowerClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-
-	payload := FlowerWorkerStatusResult{}
-	err = json.Unmarshal(body, &payload)
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-
-	return payload, nil
-}
-
-func getQueueWorkerStatus(ctx context.Context, queue string) (*FlowerWorkerStatusResult, error) {
-	start := time.Now()
-
-	queueWorkerStatusData, err := reqCache.Get(ctx, "worker_stats", loadQueueWorkerStatus)
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-
-	queueWorkerStatus := queueWorkerStatusData.(FlowerWorkerStatusResult)
-
-	log.Tracef("Fetching worker status for queue %s took %s", queue, time.Since(start).String())
-
-	return &queueWorkerStatus, nil
+	return totalWorkersAvailable, totalActiveTasks
 }
 
 func getQueueLength(ctx context.Context, queue string) (int64, error) {
 	start := time.Now()
 
-	req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("%s/api/queues/length", FlowerAddress), nil)
-	if err != nil {
-		return 0, err
-	}
-
-	resp, err := flowerClient.Do(req)
-	if err != nil {
-		return 0, err
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return 0, status.Error(codes.Internal, err.Error())
-	}
-
-	payload := FlowerQueueLength{}
-	err = json.Unmarshal(body, &payload)
-	if err != nil {
-		return 0, status.Error(codes.Internal, err.Error())
-	}
-
-	for i := range payload.ActiveQueues {
-		if payload.ActiveQueues[i].Name == queue {
-			return int64(payload.ActiveQueues[i].Messages), nil
-		}
+	listLength := RedisClient.LLen(ctx, queue)
+	if listLength.Err() != nil {
+		return 0, listLength.Err()
 	}
 
 	log.Tracef("Fetching queue length for queue %s took %s", queue, time.Since(start).String())
 
-	return 0, nil
+	return listLength.Val(), nil
 }
 
 func (e *ExternalScaler) GetMetricSpec(ctx context.Context, scaledObject *pb.ScaledObjectRef) (*pb.GetMetricSpecResponse, error) {
@@ -320,7 +184,11 @@ func (e *ExternalScaler) StreamIsActive(scaledObject *pb.ScaledObjectRef, epsSer
 }
 
 func main() {
-	logLevel := os.Getenv("LOG_LEVEL")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.SetEnvPrefix("kcs")
+	viper.AutomaticEnv()
+
+	logLevel := viper.GetString("log_level")
 	if logLevel != "" {
 		parsedLogLevel, err := log.ParseLevel(logLevel)
 		if err == nil {
@@ -330,15 +198,9 @@ func main() {
 		}
 	}
 
-	FlowerAddress = os.Getenv("FLOWER_ADDRESS")
-	FlowerAddress = strings.TrimSpace(FlowerAddress)
-	if FlowerAddress == "" {
-		log.Fatal("Env variable FLOWER_ADDRESS needs to be set to a valid URL, value is empty")
-	}
-
-	_, err := url.Parse(FlowerAddress)
+	err := connectRedis()
 	if err != nil {
-		log.Fatalf("Env variable FLOWER_ADDRESS needs to be set to a valid URL: %s", err.Error())
+		log.Fatalf("could not connect to redis: %s", err.Error())
 	}
 
 	address := ":6000"
@@ -352,24 +214,266 @@ func main() {
 	}
 }
 
-// Helper function to check load without KEDA.
-func checker() {
-	ticker := time.NewTicker(500 * time.Millisecond)
-	done := make(chan bool)
+var RedisClient *redis.Client
+
+func connectRedis() error {
+	var redisTLSConfig *tls.Config
+	if viper.GetBool("Redis.TLS.Enabled") {
+		redisTLSConfig = &tls.Config{
+			MinVersion: tls.VersionTLS12,
+			ServerName: viper.GetString("Redis.TLS.ServerName"),
+		}
+	}
+
+	if viper.GetString("Redis.Type") == "sentinel" {
+		sentinelServers := strings.Split(viper.GetString("Redis.Server"), ",")
+
+		// Automatically calculate hostname.
+		if redisTLSConfig != nil && redisTLSConfig.ServerName != "" && len(sentinelServers) > 0 {
+			firstServerParts := strings.Split(sentinelServers[0], ":")
+			firstServerName := firstServerParts[0]
+			redisTLSConfig.ServerName = firstServerName
+		}
+
+		options := &redis.FailoverOptions{
+			MasterName:       viper.GetString("Redis.Master"),
+			SentinelUsername: viper.GetString("Redis.SentinelUsername"),
+			SentinelPassword: viper.GetString("Redis.SentinelPassword"),
+			Username:         viper.GetString("Redis.Username"),
+			Password:         viper.GetString("Redis.Password"),
+			DB:               viper.GetInt("Redis.DB"),
+			SentinelAddrs:    sentinelServers,
+			TLSConfig:        redisTLSConfig,
+		}
+
+		if viper.GetInt("Redis.Timeouts.Dial") > 0 {
+			options.DialTimeout = time.Millisecond * time.Duration(viper.GetInt("Redis.Timeouts.Dial"))
+		}
+
+		if viper.GetInt("Redis.Timeouts.Read") > 0 {
+			options.ReadTimeout = time.Millisecond * time.Duration(viper.GetInt("Redis.Timeouts.Read"))
+		}
+
+		if viper.GetInt("Redis.Timeouts.Write") > 0 {
+			options.WriteTimeout = time.Millisecond * time.Duration(viper.GetInt("Redis.Timeouts.Write"))
+		}
+
+		if viper.GetInt("Redis.Connections.Min") > 0 {
+			options.MinIdleConns = viper.GetInt("Redis.Connections.Min")
+		}
+
+		if viper.GetInt("Redis.Connections.Max") > 0 {
+			options.PoolSize = viper.GetInt("Redis.Connections.Max")
+		}
+
+		RedisClient = redis.NewFailoverClient(options)
+	} else {
+		server := viper.GetString("Redis.Server")
+
+		// Automatically calculate hostname.
+		if redisTLSConfig != nil && redisTLSConfig.ServerName != "" {
+			serverParts := strings.Split(server, ":")
+			serverName := serverParts[0]
+			redisTLSConfig.ServerName = serverName
+		}
+
+		options := &redis.Options{
+			Addr:      server,
+			Username:  viper.GetString("Redis.Username"),
+			Password:  viper.GetString("Redis.Password"),
+			DB:        viper.GetInt("Redis.DB"),
+			TLSConfig: redisTLSConfig,
+		}
+
+		if viper.GetInt("Redis.Timeouts.Dial") > 0 {
+			options.DialTimeout = time.Millisecond * time.Duration(viper.GetInt("Redis.Timeouts.Dial"))
+		}
+
+		if viper.GetInt("Redis.Timeouts.Read") > 0 {
+			options.ReadTimeout = time.Millisecond * time.Duration(viper.GetInt("Redis.Timeouts.Read"))
+		}
+
+		if viper.GetInt("Redis.Timeouts.Write") > 0 {
+			options.WriteTimeout = time.Millisecond * time.Duration(viper.GetInt("Redis.Timeouts.Write"))
+		}
+
+		if viper.GetInt("Redis.Connections.Min") > 0 {
+			options.MinIdleConns = viper.GetInt("Redis.Connections.Min")
+		}
+
+		if viper.GetInt("Redis.Connections.Max") > 0 {
+			options.PoolSize = viper.GetInt("Redis.Connections.Max")
+		}
+
+		RedisClient = redis.NewClient(options)
+	}
+
+	pong := RedisClient.Ping(context.Background())
+	if pong.Err() != nil {
+		return fmt.Errorf("could not connect to redis: %w", pong.Err())
+	}
+
+	// Listen for heartbeats and process them.
+	pubsub := RedisClient.PSubscribe(context.Background(), "/*.celeryev/worker.heartbeat")
 	go func() {
+		ch := pubsub.Channel()
+		for msg := range ch {
+			processHeartbeat(msg)
+		}
+	}()
+
+	// Run worker cleaner task.
+	go func() {
+		// Clean every x seconds.
+		workerCleanupInterval := viper.GetInt("worker_cleanup_interval")
+		if workerCleanupInterval == 0 {
+			workerCleanupInterval = 5
+		}
+		ticker := time.NewTicker(time.Duration(workerCleanupInterval) * time.Second)
 		for {
 			select {
-			case <-done:
-				return
 			case <-ticker.C:
-				FlowerAddress = "http://127.0.0.1:8888"
-				load, err := getLoad(context.Background(), "celery")
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				log.Printf("The current load is %d", load)
+				cleanupWorkers()
 			}
 		}
 	}()
+
+	return nil
+}
+
+type CeleryWorker struct {
+	Hostname    string
+	LastSeen    time.Time
+	Queues      []string
+	Concurrency int64
+	Active      int64
+}
+
+type CeleryHeartbeat struct {
+	Hostname    string                     `json:"hostname"`
+	Utcoffset   int64                      `json:"utcoffset"`
+	Pid         int64                      `json:"pid"`
+	Clock       int64                      `json:"clock"`
+	Freq        float64                    `json:"freq"`
+	Active      int64                      `json:"active"`
+	Processed   int64                      `json:"processed"`
+	Loadavg     []float64                  `json:"loadavg"`
+	SwIdent     string                     `json:"sw_ident"`
+	SwVer       string                     `json:"sw_ver"`
+	SwSys       string                     `json:"sw_sys"`
+	Timestamp   float64                    `json:"timestamp"`
+	Type        string                     `json:"type"`
+	XWorkerInfo *CeleryHeartbeatWorkerInfo `json:"x_worker_info"`
+}
+
+type CeleryHeartbeatWorkerInfo struct {
+	Queues      []string `json:"queues"`
+	Concurrency int64    `json:"concurrency"`
+}
+
+var celeryWorkers = map[string]*CeleryWorker{}
+var workerMapLock sync.Mutex
+
+func updateWorker(worker CeleryHeartbeat) {
+	workerMapLock.Lock()
+	defer workerMapLock.Unlock()
+	_, ok := celeryWorkers[worker.Hostname]
+	if !ok {
+		celeryWorkers[worker.Hostname] = &CeleryWorker{
+			Hostname: worker.Hostname,
+		}
+	}
+
+	celeryWorkers[worker.Hostname].LastSeen = time.Now()
+	celeryWorkers[worker.Hostname].Active = worker.Active
+
+	if worker.XWorkerInfo != nil {
+		celeryWorkers[worker.Hostname].Queues = worker.XWorkerInfo.Queues
+		celeryWorkers[worker.Hostname].Concurrency = worker.XWorkerInfo.Concurrency
+	} else {
+		// No worker info found, map queues from hostname and set concurrency to 1.
+		celeryWorkers[worker.Hostname].Queues = getWorkerQueues(worker)
+		celeryWorkers[worker.Hostname].Concurrency = 1
+	}
+
+	d, _ := json.Marshal(celeryWorkers[worker.Hostname])
+
+	log.Debugf("Updated worker %s: %s", worker.Hostname, string(d))
+}
+
+func getWorkerQueues(worker CeleryHeartbeat) []string {
+	workerQueueMapSetting := viper.GetString("worker_queue_map")
+	if workerQueueMapSetting != "" {
+		workerQueueMaps := strings.Split(workerQueueMapSetting, ";")
+		for i := range workerQueueMaps {
+			workerQueueMap := workerQueueMaps[i]
+			workerQueueMapParts := strings.Split(workerQueueMap, ":")
+			workerQueueMapIdentifier := workerQueueMapParts[0]
+			workerQueueMapQueues := strings.Split(workerQueueMapParts[1], ",")
+			if strings.Contains(worker.Hostname, workerQueueMapIdentifier) {
+				log.Debugf("Mapped queues for worker %s to %s", worker.Hostname, workerQueueMap)
+				return workerQueueMapQueues
+			}
+		}
+	}
+
+	return []string{"celery"}
+}
+
+type CeleryHeartbeatMessage struct {
+	Body            string `json:"body"`
+	ContentEncoding string `json:"content-encoding"`
+	ContentType     string `json:"content-type"`
+	Headers         struct {
+		Hostname string `json:"hostname"`
+	} `json:"headers"`
+	Properties struct {
+		DeliveryMode int64 `json:"delivery_mode"`
+		DeliveryInfo struct {
+			Exchange   string `json:"exchange"`
+			RoutingKey string `json:"routing_key"`
+		} `json:"delivery_info"`
+		Priority     int64  `json:"priority"`
+		BodyEncoding string `json:"body_encoding"`
+		DeliveryTag  string `json:"delivery_tag"`
+	} `json:"properties"`
+}
+
+func processHeartbeat(msg *redis.Message) {
+	decodedMessage := CeleryHeartbeatMessage{}
+	err := json.Unmarshal([]byte(msg.Payload), &decodedMessage)
+	if err != nil {
+		log.Warnf("could not decode heartbeat message: %s", err.Error())
+	}
+
+	sDec, err := base64.StdEncoding.DecodeString(decodedMessage.Body)
+	if err != nil {
+		log.Warnf("could not decode heartbeat message body: %s", err.Error())
+	}
+
+	decodedHeartbeat := CeleryHeartbeat{}
+	err = json.Unmarshal(sDec, &decodedHeartbeat)
+	if err != nil {
+		log.Warnf("could not decode heartbeat: %s", err.Error())
+	}
+
+	updateWorker(decodedHeartbeat)
+}
+
+func cleanupWorkers() {
+	log.Debugf("Running worker cleanup")
+	workerMapLock.Lock()
+	defer workerMapLock.Unlock()
+
+	workerStaleTime := viper.GetFloat64("worker_stale_time")
+	if workerStaleTime == 0 {
+		workerStaleTime = 10
+	}
+
+	for i := range celeryWorkers {
+		if time.Now().Sub(celeryWorkers[i].LastSeen).Seconds() > workerStaleTime {
+			log.Debugf("Removing worker %s because it has not been since for more than %.2f seconds", i, time.Now().Sub(celeryWorkers[i].LastSeen).Seconds())
+			delete(celeryWorkers, i)
+		}
+	}
 }
